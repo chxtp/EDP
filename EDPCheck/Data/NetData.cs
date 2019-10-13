e56695a766fed9442d314cdf9179a912c57d407d
chxtp <279008336@qq.com>
2019-10-13 11:51:32 +0800

    add1

---
+++ EDPCheck/Data/NetData.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EDPCheck
+{
+    class NetData
+    {
+        EDPDataDataContext db = new EDPDataDataContext();
+
+        public string GetSTName(int stid) 
+        {
+            var st = from s in db.T_ST where s.STID == stid select s;
+            return st.FirstOrDefault().ST;
+        }
+        public T_ST GetSTInfo(int stid) 
+        {
+            var st = from s in db.T_ST where s.STID == stid select s;
+            return st.FirstOrDefault();
+        }
+
+        public static void UpdateMsg() 
+        {
+
+        }
+
+        public static void Err() 
+        {
+
+        }
+        public static void ReBack() 
+        {
+
+        }
+    }
+}
---
+++ EDPCheck/FrmMain.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EDPCheck
+{
+    public partial class FrmMain : Form
+    {
+        public FrmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmMain_Load(object sender, EventArgs e)
+        {
+            EDPCheck.Helper.FrmHelper.SetPenetrate(this);//设置窗体具有鼠标穿透效果
+            EDPCheckHelper.LocalIP = EDPCheck.Helper.NetHelper.GetLocalIP();
+        }
+
+    }
+}
